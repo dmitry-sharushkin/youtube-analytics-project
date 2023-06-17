70a7575fde77b97cca38aad932dd609d34b37c47
dmitry-sharushkin <dimasharushkin@gmail.com>
2023-06-17 11:34:22 +0300

    adding try/except

--- src/video.py
+++ src/video.py
@@ -9,11 +9,18 @@ class Video:
     def __init__(self, video_id):
         """Экземпляр инициализируется реальными данными."""
         self.video_id = video_id
-        video_response = self._get_video_info()
-        self.title = video_response['items'][0]['snippet']['title']
-        self.url = f'https://www.youtube.com/videos/{self.video_id}'
-        self.view_count = video_response['items'][0]['statistics']['viewCount']
-        self.like_count = video_response['items'][0]['statistics']['likeCount']
+
+        try:
+            video_response = self._get_video_info()
+            self.title = video_response['items'][0]['snippet']['title']
+            self.url = f'https://www.youtube.com/videos/{self.video_id}'
+            self.view_count = video_response['items'][0]['statistics']['viewCount']
+            self.like_count = video_response['items'][0]['statistics']['likeCount']
+        except:
+            self.title = None
+            self.url = None
+            self.view_count = None
+            self.like_count = None
 
     def __str__(self):
         """Выводит название канала"""
@@ -36,6 +43,5 @@ class PLVideo(Video):
     """Второй класс для видео (плейлисты)"""
 
     def __init__(self, video_id, playlist_id):
-
         super().__init__(video_id)
         self.playlist_id = playlist_id
